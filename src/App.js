fcf2469549404d5863542849f511c00e6c59817d
MaxyCopeland <max.copeland1@gmail.com>
2023-07-05 16:29:26 -0400

    useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import CampsitesList from './features/campsites/CampsitesList.js';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -8,7 +8,7 @@ function App() {
     return (
         <div className='App'>  
             <Header />        
-            <CampsitesList  />
+            <CampsitesDirectoryPage  />
             <Footer />
         </div>
     );
--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -2,7 +2,7 @@ import { Col, Row } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from  './campsitesSlice';
 
-const CampsiteList = () => {
+const CampsiteList = ({ setCampsiteId }) => {
     const campsites = selectAllCampsites();
 
     return (
@@ -10,7 +10,11 @@ const CampsiteList = () => {
             
             {campsites.map((campsite) => {
                 return (
-                    <Col md='5' className='m-4' key={campsite.id}>
+                    <Col 
+                        md='5' 
+                        className='m-4' 
+                        key={campsite.id}
+                        onClick={() => setCampsiteId(campsite.id)}>
                         <CampsiteCard campsite={campsite} />
                     </Col>
                 );
