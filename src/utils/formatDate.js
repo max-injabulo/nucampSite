c0ebaa771b684e0cb20c9941c1febd452331d86f
MaxyCopeland <max.copeland1@gmail.com>
2023-07-08 14:08:34 -0400

    added comments to campsites

---
+++ src/utils/formatDate.js
@@ -0,0 +1,11 @@
+export const formatDate = (date) => {
+    return new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit'
+    }).format(new Date(Date.parse(date)))
+
+};
+
+
+
