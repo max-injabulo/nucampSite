e42d2a72b11802ed859c0870588ef427d38dfefc
MaxyCopeland <max.copeland1@gmail.com>
2023-07-18 12:33:10 -0400

    Redux campsitesSlice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,4 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
+
+const initialState = {
+    campsitesArray: CAMPSITES
+};
+
+const campsitesSlice = createSlice({
+    name: 'campsites',
+    initialState
+});
+
+export const campsitesReducer = campsitesSlice.reducer;
 
 export const selectAllCampsites = () => {
     return CAMPSITES;
@@ -11,8 +23,3 @@ export const selectCampsiteById = (id) => {
 export const selectFeaturedCampsite = () => {
     return CAMPSITES.find((campsite) => campsite.featured);
 };
-
-
-// export const selectRandomCampsite = () => {
-//     return CAMPSITES[Math.floor(Math.random()*CAMPSITES.length)];
-// }; 
