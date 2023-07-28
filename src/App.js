b626a97507bd37d0b6e4bc30734157630f9c0ecd
MaxyCopeland <max.copeland1@gmail.com>
2023-07-28 12:30:53 -0400

    added Async Promotions

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage'
 import { fetchCampsites } from './features/campsites/campsitesSlice';
 import { fetchPartners } from './features/partners/partnerSlice';
+import { fetchPromotions } from './features/promotions/promotionsSlice';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
       useEffect(() => {
         dispatch(fetchCampsites());
         dispatch(fetchPartners());
+        dispatch(fetchPromotions());
     }, [dispatch]);
 
     return (
